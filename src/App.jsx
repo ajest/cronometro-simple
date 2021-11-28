a5a33473b387d3360a467070212c8b2efc81729f
Pablo Fumarola <pablo.fumarola@gmail.com>
2021-11-28 11:20:32 -0300

    Add css flex property

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css'
-
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Cronómetro Simple</p>
-      </header>
+    <div className="container">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, neque facere vitae non, consectetur quod iure veniam et, est impedit tempore cumque ratione quae qui numquam ipsum. Velit, facere molestiae.</p>
     </div>
   )
 }
